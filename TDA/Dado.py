8e859e920c74c9685e80c0a326f2fda0e2bdb973
pablo <juan.banda2211@gmail.com>
2019-10-14 11:09:35 -0500

    First commit

---
+++ TDA/Dado.py
@@ -0,0 +1,14 @@
+import random
+
+class dado:
+
+    def __init__(self):
+        self.dado = o
+
+    def tirarDado(self):
+        self.dado = rsndom.randint(1 ,6)
+
+    def  getDado(self):
+        return self.dado
+
+    
